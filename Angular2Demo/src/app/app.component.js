2f4715dfbf7e0860a23aab3cf35947d959c1de1f
phurba <mail.phurba@gmail.com>
2017-07-11 11:37:44 +0800

    First Hello World

--- Angular2Demo/src/app/app.component.js
+++ Angular2Demo/src/app/app.component.js
@@ -9,12 +9,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular 2';
+        this.name = "Phurba";
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1>",
+            template: '<h1>Hello {{name}} </h1>'
         })
     ], AppComponent);
     return AppComponent;
--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector : 'my-app',
+    template: '<h1>Hello {{name}} </h1>'
 })
-export class AppComponent  { name = 'Angular 2'; }
+
+export class AppComponent {
+    name: string = "Phurba";
+}
